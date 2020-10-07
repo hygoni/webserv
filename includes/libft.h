62f9b2221768f6ed8e34c23fd99f217093af9bca
hygoni <9hrack@naver.com>
2020-10-07 22:01:01 +0900

    [FIX] fix invalid variable name

--- includes/libft.h
+++ includes/libft.h
@@ -30,8 +30,8 @@ typedef struct	s_list
 t_list			*ft_lstnew(void *content);
 t_list			*ft_lstlast(t_list *lst);
 t_list			*ft_lstmap(t_list *lst, void *(*f)(void*), void (*del)(void *));
-void			ft_lstadd_front(t_list **lst, t_list *new);
-void			ft_lstadd_back(t_list **lst, t_list *new);
+void			ft_lstadd_front(t_list **lst, t_list *new_list);
+void			ft_lstadd_back(t_list **lst, t_list *new_list);
 void			ft_lstdelone(t_list *lst, void (*del)(void *));
 void			ft_lstclear(t_list **lst, void (*del)(void *));
 void			ft_lstiter(t_list *lst, void (*f)(void *));
