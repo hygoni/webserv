d5e95722ab0e45d44cf783ab88c277f50e35df5b
hygoni <9hrack@naver.com>
2020-10-11 20:58:57 +0900

    [FIX] fix error on abnf parser

--- srcs/construct.cpp
+++ srcs/construct.cpp
@@ -12,84 +12,78 @@ void end(Context& ctx) {
     ctx.level--;
 	if ((unsigned long)(ctx.idx) != ctx.str.size())
 		ctx.state = false;
+	ctx.save.pop();
 }
 
 void group(Context& ctx) {
-    if (ctx.state) {
-        ctx.level++;
-        ctx.save.push(ctx.idx);
-    }
+    if (!ctx.state)
+		return;
+	ctx.level++;
+	ctx.save.push(ctx.idx);
 }
 
 void ungroup(Context& ctx) {
+	if (!ctx.state)
+		return ;
     if (ctx.skip == ctx.level) {
         ctx.skip = INT_MAX;
-    }
-	if (ctx.state == false) {
-        ctx.idx = ctx.save.top(); 
     }
 	ctx.save.pop();
     ctx.level--;
 }
 
 void repeat(Context& ctx, int min, int max, bool (*f)(Context&)) {
-    if (ctx.skip <= ctx.level) {
+    if (ctx.skip <= ctx.level || !ctx.state) {
         return;
     }
-    if (ctx.state) {
-        int i;
-        /* max == 0이면 실패할 때까지 */
-        for (i = 0; max == 0 ? true : i < max; i++) {
-            ctx.save.push(ctx.idx);
-			ctx.state = f(ctx);
-            if (ctx.state == false) {
-                ctx.idx = ctx.save.top(); ctx.save.pop();
-                break;
-            }
-			ctx.save.pop();
-        }
-        ctx.state = ((i >= min) && (max == 0 ? true :  i <= max));
-    }
+	int i;
+	/* max == 0이면 실패할 때까지 */
+	for (i = 0; max == 0 ? true : i < max; i++) {
+		ctx.save.push(ctx.idx);
+		ctx.state = f(ctx);
+		if (ctx.state == false) {
+			ctx.idx = ctx.save.top(); ctx.save.pop();
+			break;
+		}
+		ctx.save.pop();
+	}
+	ctx.state = ((i >= min) && (max == 0 ? true :  i <= max));
 }
 
 void repeat(Context& ctx, char ch, int min, int max, bool (*f)(Context&, char)) {
-    if (ctx.skip <= ctx.level) {
+    if (ctx.skip <= ctx.level || !ctx.state) {
         return;
     }
-    if (ctx.state) {
-        int i;
-        /* max == 0이면 실패할 때까지 */
-        for (i = 0; max == 0 ? true : i < max; i++) {
-            ctx.save.push(ctx.idx);
-			ctx.state = f(ctx, ch);
-            if (ctx.state == false) {
-                ctx.idx = ctx.save.top(); ctx.save.pop();
-                break;
-            }
-			ctx.save.pop();
-        }
-        ctx.state = ((i >= min) && (max == 0 ? true :  i <= max));
-    }
+	int i;
+	/* max == 0이면 실패할 때까지 */
+	for (i = 0; max == 0 ? true : i < max; i++) {
+		ctx.save.push(ctx.idx);
+		ctx.state = f(ctx, ch);
+		if (ctx.state == false) {
+			ctx.idx = ctx.save.top(); ctx.save.pop();
+			break;
+		}
+		ctx.save.pop();
+	}
+	ctx.state = ((i >= min) && (max == 0 ? true :  i <= max));
 }
 
 void repeat(Context& ctx, bool b, int min, int max, bool (*f)(Context&, bool)) {
-    if (ctx.skip <= ctx.level) {
+    if (ctx.skip <= ctx.level || !ctx.state) {
         return;
     }
-    if (ctx.state) {
-        int i;
-        /* max == 0이면 실패할 때까지 */
-        for (i = 0; max == 0 ? true : i < max; i++) {
-            ctx.save.push(ctx.idx);
-            ctx.state = f(ctx, b);
-            if (ctx.state == false) {
-                ctx.idx = ctx.save.top(); ctx.save.pop();
-                break;
-            }
-			ctx.save.pop();
-        }
-        ctx.state = ((i >= min) && (max == 0 ? true :  i <= max));
-    }
+	int i;
+	/* max == 0이면 실패할 때까지 */
+	for (i = 0; max == 0 ? true : i < max; i++) {
+		ctx.save.push(ctx.idx);
+		ctx.state = f(ctx, b);
+		if (ctx.state == false) {
+			ctx.idx = ctx.save.top(); ctx.save.pop();
+			break;
+		}
+		ctx.save.pop();
+	}
+	ctx.state = ((i >= min) && (max == 0 ? true :  i <= max));
 }
 
 void option(Context& ctx, bool (*f)(Context&)) {
@@ -99,21 +93,21 @@ void option(Context& ctx, bool (*f)(Context&)) {
 }
 
 void accept(Context& ctx, bool (*f)(Context&)) {
-    if (ctx.skip <= ctx.level) {
+    if (ctx.skip <= ctx.level || !ctx.state) {
         return;
     }
     ctx.state = f(ctx);
 }
 
 void accept(Context& ctx, char ch, bool (*f)(Context&, char)) {
-    if (ctx.skip <= ctx.level) {
+    if (ctx.skip <= ctx.level || !ctx.state) {
         return;
     }
     ctx.state = f(ctx, ch);
 }
 
 void accept(Context& ctx, bool b, bool (*f)(Context&, bool)) {
-    if (ctx.skip <= ctx.level) {
+    if (ctx.skip <= ctx.level || !ctx.state) {
         return;
     }
     ctx.state = f(ctx, b);
@@ -126,7 +120,7 @@ void alternate(Context& ctx) {
     if (ctx.state) {
         ctx.skip = ctx.level;
     } else {
-        ctx.idx = ctx.save.top(); ctx.save.pop();
+        ctx.idx = ctx.save.top();
         ctx.state = true;
     }
 }
