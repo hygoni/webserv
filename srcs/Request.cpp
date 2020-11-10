2d140ae8e587851015c76a13280e0581646c7422
DiceMono <qrdfg@naver.com>
2020-11-10 20:28:50 +0900

    [UPDATE] update request class

--- srcs/Request.cpp
+++ srcs/Request.cpp
@@ -1,71 +1,106 @@
 #include "Request.hpp"
-#include "abnf.hpp"
-#include "cbnf.hpp"
 #include "libft.h"
 #include "construct.hpp"
-// method SP request-target SP HTTP-version CRLF
-// field-name ":" OWS field-value OWS
+// HTTP-message   = start-line
+//                           *( header-field CRLF )
+//                           CRLF
+//                           [ message-body ]
+// start-line = method SP request-target SP HTTP-version CRLF
+// header-field = field-name ":" OWS field-value OWS
 
-void  Request::initMethod(Context& ctx) {
-  size_t start = ctx.current();
-  CBNF::Token(ctx);
-  this->_method = ctx.str.substr(start, ctx.current());
+void    Request::initMethod(std::string method) {
+  this->_method = method;
 }
 
-void  Request::initTarget(Context& ctx) {
-  //  request-target = origin-form / absolute-form / authority-form / asterisk-form
+void    Request::initTarget(std::string target) {
+  if (target.length() > 8000)
+    throw HttpException(414);
+  this->_target = target;
+}
 
-  //  origin-form = absolute-path [ "?" query ]
-  //    absolute-path = 1*( "/" segment )
-  //      segment = *pchar
-  //    query = *( pchar / "/" / "?" )
+void    Request::initVersion(std::string version) {
+  if (version != "HTTP/1.1")
+    throw HttpException(400);
+  this->_version = version;
+}
 
-  //  absolute-form = absolute-URI
-  //    absolute-URI = scheme ":" hier-part [ "?" query ]
-  //      scheme        = ALPHA *( ALPHA / DIGIT / "+" / "-" / "." )
-  //      hier-part   = "//" authority path-abempty / path-absolute / path-rootless / path-empty
-  //        authority     = [ userinfo "@" ] host [ ":" port ]
-  //          userinfo      = *( unreserved / pct-encoded / sub-delims / ":" )
-  //          host          = IP-literal / IPv4address / reg-name
-  //            IP-literal = "[" ( IPv6address / IPvFuture  ) "]"
-  //              IPv6address = !!!!!!!!!!!!!!!!!!!!!
-  //              IPvFuture  = "v" 1*HEXDIG "." 1*( unreserved / sub-delims / ":" )
-  //            IPv4address = dec-octet "." dec-octet "." dec-octet "." dec-octet
-  //              dec-octet = !!!!!!!!!!!!!!!!!!!!!
-  //            reg-name = *( unreserved / pct-encoded / sub-delims )
-  //          port = *DIGIT
-  //        path-abempty = *( "/" segment )
-  //        path-absolute = "/" [ segment-nz *( "/" segment ) ]
-  //          segment-nz = 1*pchar
-  //        path-rootless = segment-nz *( "/" segment )
-  //        path-empty    = 0<pchar>
+void    Request::initStartLine(std::string raw) {
+  size_t  start = 0;
+  size_t  space_count = 0;
 
-  //  authority-form = authority
+  for (size_t i = 0; raw.length() > 0 && i < raw.length() - 1; i++) {
+    if (raw.at(i) == ' ') {
+      space_count++;
+      if (space_count == 1) {
+        this->initMethod(raw.substr(start, i - start));
+      } else if (space_count == 2) {
+        this->initTarget(raw.substr(start, i - start));
+        this->initVersion(raw.substr(i + 1));
+        return ;
+      } 
+      start = i + 1;
+    }
+  }
+}
 
-  //  asterisk-form = "*"
+std::pair<std::string, std::string>    Request::makeHeader(std::string header_field) {
+  size_t  sep_i = header_field.find(":");
+  std::string field_name, field_value;
+  if (sep_i == std::string::npos)
+    throw HttpException(400);
+  field_name = header_field.substr(0, sep_i);
+  if (field_name.length() < 1 || field_name.front() == ' ' || field_name.back() == ' ')
+    throw HttpException(400);
+  if (field_name == "Host" && _headers.find("Host") != _headers.end())
+    throw HttpException(400);
+  field_value = std::string("");
+  if (sep_i != header_field.length() - 1) {
+    field_value = header_field.substr(sep_i + 1);
+    if (field_value.at(0) == ' ')
+      field_value = field_value.substr(1);
+  }
+  return (std::make_pair(field_name, field_value));
+}
+
+void    Request::initHeaders(std::string raw) {
+  size_t      start = 0;
+  std::string header_field;
+
+  for (size_t i = 0; i < raw.length() - 1; i++) {
+    if (raw.at(i) == '\r' && raw.at(i + 1) == '\n') {
+      if (start == i)
+        return ;
+      header_field = raw.substr(start, i - start);
+      this->_headers.insert(makeHeader(header_field));
+      start = i + 2;
+    }
+  }
+  throw HttpException(400);
 }
 
 Request::Request(std::string http_message) {
-  Context ctx(http_message);
-
-  begin(ctx);
-  CBNF::Token(ctx);
-  accept(ctx, ABNF::Sp);
-
-
-  accept(ctx, ABNF::Sp);
-  accept(ctx, 'H', ABNF::Scar);
-  accept(ctx, 'T', ABNF::Scar);
-  accept(ctx, 'T', ABNF::Scar);
-  accept(ctx, 'P', ABNF::Scar);
-  accept(ctx, '/', ABNF::Scar);
-  accept(ctx, ABNF::Digit);
-  accept(ctx, '.', ABNF::Icar);
-  accept(ctx, ABNF::Digit);
-  accept(ctx, false, ABNF::Crlf);
-  if (ctx.state == false)
-    throw std::logic_error("400");
-
-  end(ctx);
+  size_t start_line_crlf = http_message.find("\r\n");
+  if (start_line_crlf == std::string::npos)
+    throw HttpException(400);
+  initStartLine(http_message.substr(0, start_line_crlf));
+  initHeaders(http_message.substr(start_line_crlf + 2));
+  if (_headers.find("Host") == _headers.end())
+    throw HttpException(400);
 }
 
+void    Request::debugOstream(std::ostream& os) const {
+  os << "-----Debug Request-----\n" \
+  << "Method: \"" << _method << "\"\n" \
+  << "Target: \"" << _target << "\"\n" \
+  << "Version: \"" << _version << "\"\n" \
+  << "---------Header--------\n";
+  std::map<std::string, std::string>::const_iterator it;
+  for (it = _headers.begin(); it != _headers.end(); it = std::next(it))
+    os << it->first << ": \"" << it->second << "\"\n"; 
+  os << "-----------------------\n";
+}
+
+std::ostream& operator<<(std::ostream& os, const Request& request) {
+  request.debugOstream(os);
+  return os;
+}
--- srcs/Request.hpp
+++ srcs/Request.hpp
@@ -1,20 +1,29 @@
 #ifndef REQUEST_HPP_
 #define REQUEST_HPP_
 #include <string>
+#include <iostream>
+#include <map>
+#include "HttpException.hpp"
 /* Copyright 2020 hyeyoo, hyekim */
-// ctx.state = ((i >= min) && (max == 0 ? true :  i <= max)); ?? repeat
+
 class Request {
 private:
   std::string _method;
   std::string _target;
-  float       _version;
+  std::string _version;
+  std::map<std::string, std::string>  _headers;
 
   Request();
-  void  initMethod(Context& ctx);
-  void  initTarget(Context& ctx);
-  void  initVersion(Context& ctx);
+  std::pair<std::string, std::string> makeHeader(std::string header_field);
+  void  initMethod(std::string raw);
+  void  initTarget(std::string raw);
+  void  initVersion(std::string raw);
+  void  initStartLine(std::string raw);
+  void  initHeaders(std::string raw);
 public:
   Request(std::string raw);
+  void  debugOstream(std::ostream& os) const;
 };
 
+std::ostream& operator<<(std::ostream& os, const Request& request);
 #endif
