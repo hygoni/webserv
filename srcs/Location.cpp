e016dba3b349855cb717cc6639b88089e2ab0621
hygoni <9hrack@naver.com>
2020-11-12 01:29:32 +0900

    [UPDATE] update Server, Location spec

--- srcs/Location.cpp
+++ srcs/Location.cpp
@@ -3,22 +3,29 @@
 #include "Location.hpp"
 #include <utility>
 
-Location::Location() {}
-
-void Location::setAttribute
-(std::string const& key, std::string const& value) {
-    this->_attrs.insert(std::make_pair(key, value));
-}
-
-void Location::setPath(std::string const& path) {
+Location::Location
+(std::string const& path, std::string const& root) {
     this->_path = path;
-}
-
-std::map<std::string, std::string>::const_iterator Location::getAttribute
-(std::string const& key) const {
-    return this->_attrs.find(key);
+    this->_root = root;
 }
 
 std::string const& Location::getPath() const {
     return this->_path;
 }
+
+std::string const& Location::getRoot() const {
+    return this->_root;
+}
+
+std::vector<std::string> const& Location::getIndex() const {
+    return this->_index;
+}
+
+void Location::addIndex(std::string const& index) {
+    this->_index.push_back(index);
+}
+
+void Location::setIndex(std::vector<std::string> index) {
+    this->_index = index;
+}
+
--- srcs/Location.hpp
+++ srcs/Location.hpp
@@ -3,21 +3,22 @@
 
 /* Copyright 2020 hyeyoo */
 
-#include <map>
+#include <vector>
 #include <string>
 
 class Location {
  private:
     std::string                         _path;
-    std::map<std::string, std::string>  _attrs;
+    std::string                         _root;
+    std::vector<std::string>            _index;
  public:
-                            Location();
-    std::map<std::string, std::string>::const_iterator
-                            getAttribute(std::string const& key) const;
-    std::string const&      getPath() const;
-    void                    setPath(std::string const& path);
-    void                    setAttribute
-                            (std::string const& key, std::string const& value);
+                                    Location
+                                    (std::string const& path, std::string const& root);
+    std::string const&              getPath() const;
+    std::string const&              getRoot() const;
+    std::vector<std::string> const& getIndex() const;
+    void                            addIndex(std::string const& index);
+    void                            setIndex(std::vector<std::string> index);
 };
 
 #endif  // SRCS_LOCATION_HPP_
